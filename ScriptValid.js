1fadc87fa428344cf77ba1c2068190703bf9afb5
GTgonzalez <95381690+GTgonzalez@users.noreply.github.com>
2022-02-07 17:13:30 -0600

    Cambios en validaciones de INDEX & REGISTRO, campos validados y codigo comentado

--- ScriptValid.js
+++ ScriptValid.js
@@ -2,41 +2,95 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 const expresiones = {
+    name: /^[a-zA-ZÀ-ÿ\s]{1,20}$/, // Letras y espacios, pueden llevar acentos.
     password: /^.{4,12}$/,
-    email: /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/
+    password2:/^.{4,12}$/,
+    email: /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/,
+    telefono: /^\d{8,8}$/ ,
+    dpi: /^\d{13,13}$/ 
 }
 /*Variables*/
 const campos = {
+    name: false,
 	password: false,
+    password2: false,
 	email: false,
+    telefono: false,
+    dpi: false    
 }
-/*Validación del formulario*/
+
+/*Validación de Login / Registro*/
 const validarFormulario = (e) => {
     switch (e.target.name){
+        /*Validación de Nombre y apellido*/
+        case "name":
+            if(expresiones.name.test(e.target.value)){
+                document.getElementById('f-nombre').classList.remove('form-group-corecto-incorrecto')
+                document.getElementById('f-nombre').classList.add('form-group-corecto') 
+            } else {
+                document.getElementById('f-nombre').classList.add('form-group-corecto')
+                document.getElementById('f-nombre').classList.remove('form-group-corecto-incorrecto')
+            }
+        break;
+        /*Validación de Password */
+        case "password":
+            validarPassword();
+        break;
+        /*Validación de Password */
+        case "password2":
+            validarPassword();
+        break;                     
+        /*Validación de email*/
         case "email":
         if(expresiones.email.test(e.target.value)) {  
             document.getElementById('idForm').classList.remove('form-group-corecto-incorrecto')
             document.getElementById('idForm').classList.add('form-group-corecto')  
-            $(function () {
-                $('[data-toggle="tooltip"]').tooltip()
-              })
-              campos['email'] = true;
         } else {
             document.getElementById('idForm').classList.add('form-group-corecto-incorrecto')
             document.getElementById('idForm').classList.remove('form-group-corecto') 
-            campos['email'] = false;   
         }
-
-    }
+        break;
+        /*Validación de Telefono */
+        case "telefono":
+        if(expresiones.telefono.test(e.target.value)){
+            document.getElementById('f-tel').classList.remove('form-group-corecto-incorrecto')
+            document.getElementById('f-tel').classList.add('form-group-corecto')            
+        } else {
+            document.getElementById('f-tel').classList.add('form-group-corecto')    
+            document.getElementById('f-tel').classList.remove('form-group-corecto-incorrecto')
+        }
+        break; 
+        /*Validación de DPI */
+        case "dpi":
+        break;         
+    }    
 }
+/*Verificar contraseñas iguales*/
+function validarPassword() {
+    password = document.getElementById('password')
+    password2 = document.getElementById('password2')
+   
+   if (password.value != password2.value){
+       document.getElementById('f-pass').classList.remove('form-group-corecto') 
+       document.getElementById('f-pass').classList.add('form-group-corecto-incorrecto') 
+       document.getElementById('f-pass2').classList.remove('form-group-corecto') 
+       document.getElementById('f-pass2').classList.add('form-group-corecto-incorrecto') 
+   } else {
+       document.getElementById('f-pass2').classList.remove('form-group-corecto-incorrecto') 
+       document.getElementById('f-pass2').classList.add('form-group-corecto') 
+       document.getElementById('f-pass').classList.remove('form-group-corecto-incorrecto')
+       document.getElementById('f-pass').classList.add('form-group-corecto')
+   }
+}
+
+/*Selección fuera de campos o dentro de ellos*/
 inputs.forEach((input) =>{
     input.addEventListener('keyup', validarFormulario);
     input.addEventListener('blur', validarFormulario);
 });
-
+/*No enviar informacion en URL*/
 formulario.addEventListener('button', (e) => {
     e.preventDefault();
-
     if (campos.email  && campos.password ){
         document.getElementById('idForm').classList.remove('form-group-corecto');
         document.getElementById('grupo__password').classList.remove('form-group-corecto');
@@ -47,31 +101,3 @@ formulario.addEventListener('button', (e) => {
         document.getElementById('grupo__password').classList.add('form-group-corecto-incorrecto')
     }
 });
-
-/*Validar si los campos son iguales*/
-function verificarPasswords() {
- 
-    // Ontenemos los valores de los campos de contraseñas 
-    pass1 = document.getElementById('pass1');
-    pass2 = document.getElementById('pass2');
- 
-    // Verificamos si las constraseñas no coinciden 
-    if (pass1.value != pass2.value) {
- 
-        // Si las constraseñas no coinciden mostramos un mensaje 
-        document.getElementById("error").classList.add("mostrar"); 
-        return false;                
-    }else if (pass1.length > 8)
-    this.value = this.value.slice(0,8); 
-    else {
- 
-        // Si las contraseñas coinciden ocultamos el mensaje de error
-        document.getElementById("error").classList.remove("mostrar");
- 
-        // Mostramos un mensaje mencionando que las Contraseñas coinciden 
-        document.getElementById("ok").classList.remove("ocultar");
- 
-        // envío del formulario 
-        return true;
-    } 
-}
